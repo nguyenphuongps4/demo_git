f070c3665d477b915298d6d0e69a4daf734d7c6c
nguyenphuongps4 <nguyenphuongps4@gmail.com>
2024-01-26 13:01:55 +0700

    Initial commit

---
+++ src/test/java/fit/set2/demo_git/DemoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.set2.demo_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
